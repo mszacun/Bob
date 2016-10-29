c49a969cb04771968cad3445ca313fb2a0116546
Marcin Szachun <mszachun@gmail.com>
2016-10-29 17:54:50 +0200

    Introduce Message class

--- bob.py
+++ bob.py
@@ -4,6 +4,16 @@ import npyscreen
 from datetime import datetime
 
 
+class Message(object):
+    def __init__(self, content, sender=None, time=None):
+        self.content = content
+        self.sender = sender or 'You'
+        self.time = time or datetime.now()
+
+    def __str__(self):
+        return '{} - {}: {}'.format(self.time.strftime('%H:%M:%S'), self.sender, self.content)
+
+
 class MessageHighlightTextfield(npyscreen.Textfield):
     MESSAGE_REGEXP = re.compile('(\d+:\d+:\d+ - )(\w+: )(.*)')
 
@@ -35,7 +45,7 @@ class SendMessageActionController(npyscreen.ActionControllerSimple):
         self.add_action('.*', self.send_message, False)
 
     def send_message(self, command_line, widget_proxy, live):
-        self.parent.wMain.values.append('17:20:35 - You: ' + command_line)
+        self.parent.wMain.values.append(Message(command_line))
         self.parent.wMain.display()
 
 
