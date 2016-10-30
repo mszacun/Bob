2e6dac226cbf38902456fb0d46b0dea9ff8d80b9
Marcin Szachun <mszachun@gmail.com>
2016-10-30 15:51:12 +0100

    Add file to run bob

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+NUMBER_OF_PANES=`tmux list-panes | wc -l`
+if [ "$NUMBER_OF_PANES" -lt "3" ]; then
+    tmux split-window -h -t:.0
+    tmux split-window -t:.1
+fi
+
+tmux send-keys -t :.1 "(cd ~/bob; python2 bob.py -l)"
+tmux send-keys -t :.1 Enter
+
+tmux send-keys -t :.2 "(cd ~/bob; python2 bob.py)"
+tmux send-keys -t :.2 Enter
