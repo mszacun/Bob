6b6e4995f3e3cb4701fa787aff999f0478179f01
Marcin Szachun <marcin.szachun@nokia.com>
2016-11-02 19:54:18 +0100

    Highlight file transmissions in history pager

--- gui/highlightning.py
+++ gui/highlightning.py
@@ -7,26 +7,40 @@ from gui.popups import MessageDetailsPopup
 
 class MessageHighlightTextfield(npyscreen.Textfield):
     MESSAGE_REGEXP = re.compile('(\d+:\d+:\d+ - )(\w+: )(.*)')
+    FILE_TRANSFER_REGEXP = re.compile('(\d+:\d+:\d+)( ! -> File .*)')
 
     def __init__(self, *args, **kwargs):
         super(MessageHighlightTextfield, self).__init__(*args, **kwargs)
 
         self.syntax_highlighting = True
+        self.green  = self.parent.theme_manager.findPair(self, 'IMPORTANT')
+        self.yellow = self.parent.theme_manager.findPair(self, 'WARNING')
+        self.blue = self.parent.theme_manager.findPair(self, 'NO_EDIT')
+        self.red = self.parent.theme_manager.findPair(self, 'DANGER')
 
     def update_highlighting(self, start, end):
         match = self.MESSAGE_REGEXP.match(self.value)
-
-        green  = self.parent.theme_manager.findPair(self, 'IMPORTANT')
-        yellow = self.parent.theme_manager.findPair(self, 'WARNING')
-        blue = self.parent.theme_manager.findPair(self, 'NO_EDIT')
-
         if match:
-            sender_color = blue if match.group(2) == 'You: ' else green
+            self._highlight_message(match)
 
-            highlight = [yellow for _ in range(len(match.group(1)))]
-            highlight += [sender_color for _ in range(len(match.group(2)))]
+        match = self.FILE_TRANSFER_REGEXP.match(self.value)
+        if match:
+            self._highlight_file_transfer(match)
 
-            self._highlightingdata = highlight
+
+    def _highlight_message(self, match):
+        sender_color = self.red if match.group(2) == 'You: ' else self.green
+
+        highlight = [self.yellow for _ in range(len(match.group(1)))]
+        highlight += [sender_color for _ in range(len(match.group(2)))]
+
+        self._highlightingdata = highlight
+
+    def _highlight_file_transfer(self, match):
+        highlight = [self.yellow for _ in range(len(match.group(1)))]
+        highlight += [self.blue for _ in range(len(match.group(2)))]
+
+        self._highlightingdata = highlight
 
 
 class MessageHighlightMultiLine(npyscreen.MultiLine):
