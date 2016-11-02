fba3d02ba8e8a27f03db77fbd001850303791fd9
Marcin Szachun <marcin.szachun@nokia.com>
2016-11-02 18:16:39 +0100

    Synchronize scrolling in plaintext and ciphertext pagers

--- gui/popups.py
+++ gui/popups.py
@@ -33,8 +33,52 @@ class CaesarEncryptionConfigurationPopup(ConfirmCancelPopup):
         return self.shift_slider.value
 
 
+class SynchronizedPager(npyscreen.Pager):
+    def h_scroll_line_up(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_scroll_line_up(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_scroll_line_up(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+    def h_scroll_line_down(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_scroll_line_down(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_scroll_line_down(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+    def h_show_beginning(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_show_beginning(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_show_beginning(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+    def h_show_end(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_show_end(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_show_end(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+    def h_scroll_page_down(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_scroll_page_down(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_scroll_page_down(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+    def h_scroll_page_up(self, ch, synchronize=True):
+        super(SynchronizedPager, self).h_scroll_page_up(ch)
+
+        if synchronize:
+            self.keep_synchronized_with.entry_widget.h_scroll_page_up(ch, synchronize=False)
+            self.keep_synchronized_with.entry_widget.display()
+
+
 class PagerBoxed(npyscreen.BoxTitle):
-    _contained_widget = npyscreen.Pager
+    _contained_widget = SynchronizedPager
 
 
 class MessageDetailsPopup(npyscreen.Popup):
@@ -51,8 +95,12 @@ class MessageDetailsPopup(npyscreen.Popup):
         self.add(npyscreen.TitleFixedText, name='Author', value=self.message.sender)
         self.add(npyscreen.TitleFixedText, name='Time', value=self.message.formatted_time)
         self.add(npyscreen.TitleFixedText, name='Encryption', value=str(self.message.encryption))
-        self.add(PagerBoxed, name='Plaintext', values=self.message.plaintext.split('\n'), max_height=7, rely=6)
-        self.add(PagerBoxed, name='Ciphertext', values=self.message.ciphertext.split('\n'), max_height=7, rely=14)
+        self.plaintext = self.add(PagerBoxed, name='Plaintext', values=self.message.plaintext.split('\n'),
+                                  max_height=7, rely=6)
+        self.ciphertext = self.add(PagerBoxed, name='Ciphertext', values=self.message.ciphertext.split('\n'),
+                                   max_height=7, rely=14)
+
+        self.ciphertext.entry_widget.keep_synchronized_with = self.plaintext
 
 
     def edit(self):
@@ -65,6 +113,7 @@ class ProcessBar(npyscreen.SliderPercent):
     def __init__(self, *args, **keywords):
         super(ProcessBar, self).__init__(*args, **keywords)
         self.editable = False
+
 
 class ProcessBarBox(npyscreen.BoxTitle):
     _contained_widget = ProcessBar
